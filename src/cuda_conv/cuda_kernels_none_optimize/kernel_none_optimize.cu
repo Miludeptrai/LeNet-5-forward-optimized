7531ed272e1db1eb162a24629a702984b5cdb2d8
someone <someone@somewhere.com>
2024-01-06 17:29:30 +0700

    update non-optimize kernel

--- src/cuda_conv/cuda_kernels_none_optimize/kernel_none_optimize.cu
+++ src/cuda_conv/cuda_kernels_none_optimize/kernel_none_optimize.cu
@@ -102,7 +102,7 @@ __global__ void unroll_kernel_3(int channel_in, int height_in, int width_in, int
 
         for (int p=0;p<height_kernel;p++){
             for (int q=0;q<width_kernel;q++){
-                if((row_in -p) < height_out && col_in - q < width_out)
+                if((row_in -p) < height_out && col_in - q < width_out && (row_in -p) >= 0 && col_in - q >= 0)
                     unroll_matrix[batch_idx*width_unroll*height_unroll + (c * width_kernel * height_kernel + p*width_kernel + q)*height_unroll+ (row_in -p) *width_out + col_in - q] = in_value;
             }
         }
