6466840a2d815786019d0e64c2c2d508b3bd54bc
someone <someone@somewhere.com>
2024-01-06 15:01:23 +0700

    update non-optimize kernel

--- src/cuda_conv/cuda_kernels_none_optimize/kernel_none_optimize.cu
+++ src/cuda_conv/cuda_kernels_none_optimize/kernel_none_optimize.cu
@@ -7,8 +7,11 @@ __global__ void unroll_kernel_1(int channel_in, int height_in, int width_in, int
                             int width_kernel, int height_out, int width_out, 
                             float* X, float* X_unroll)
 {
-    int t = blockIdx.x * blockDim.x + threadIdx.x; //
+    int batch_idx = blockIdx.z;
+
+    int t = blockIdx.x * blockDim.x + threadIdx.x;//
     int height_unroll = height_out * width_out; //2
+    int width_unroll = height_kernel * width_kernel * channel_in; //2
     if(t < channel_in*height_unroll)
     {   
         //output is a vector size : imagearea x (kernalarea * channel_in)
@@ -23,7 +26,7 @@ __global__ void unroll_kernel_1(int channel_in, int height_in, int width_in, int
         int col_out = row_unroll % width_out;//
 
         //channel start position 
-        int start_c = c*(width_in*height_in);//
+        int start_c =batch_idx*channel_in*height_in*width_in c*(width_in*height_in);//
 
         //how many rows of the channel before this?
         int w_base =  c * width_kernel * height_kernel; //
@@ -36,7 +39,7 @@ __global__ void unroll_kernel_1(int channel_in, int height_in, int width_in, int
                 //Attention, in spite of each channel (vector) store data in row-major, 
                 //But our output is a matrix, so we need to perform storing in col-major
                 //I hate this =.= 
-                X_unroll[col_unroll*height_unroll + row_unroll] = X[start_c + f_row + f_col];
+                X_unroll[batch_idx*width_unroll*height_unroll + col_unroll*height_unroll + row_unroll] = X[start_c + f_row + f_col];
             }
         }
     }
@@ -73,14 +76,16 @@ int t = blockIdx.x * blockDim.x + threadIdx.x; //
 __global__ void multi_weight_add_bias_kernel_1(float* unroll_matrix, float *weight_data, float* output_data,float* bias_data,
                                                 int height_unroll, int width_unroll,int channel_out)
 {
+    
+    int batch_idx = blockIdx.z;
 	int c = blockIdx.x * blockDim.x + threadIdx.x; 
 	int r = blockIdx.y * blockDim.y + threadIdx.y; 
 	if (r < height_unroll && c < channel_out) {
     float sum = 0 ;
     for (int i = 0; i < width_unroll ; i++) { 
-      sum += unroll_matrix[i*height_unroll + r] * weight_data[c*width_unroll + i];
+      sum += unroll_matrix[batch_idx*width_unroll*height_unroll + i*height_unroll + r] * weight_data[c*width_unroll + i];
     }
-    output_data[c*height_unroll + r] = sum + bias_data[c]; 
+    output_data[batch_idx*height_unroll*channel_out + c*height_unroll + r] = sum + bias_data[c]; 
 
   } 
 }
@@ -96,53 +101,81 @@ __host__ void Kernel_none_optimize::cuda_conv_forward( int n_samples,  int chann
     const int width_out = width_in - width_kernel + 1;
 
     // Allocate device memory
-    float *device_input, *device_output, *device_weight,*device_bias, *device_unroll_matrix; 
-    CHECK(cudaMalloc((void **)&device_input, n_samples * channel_in * height_in * width_in * sizeof(float)));
-    CHECK(cudaMalloc((void **)&device_output, n_samples * channel_out * height_out * width_out * sizeof(float)));
+    float *device_weight,*device_bias, *device_unroll_matrix; 
     CHECK(cudaMalloc((void **)&device_weight, channel_out * channel_in * height_kernel * width_kernel * sizeof(float)));
     CHECK(cudaMalloc((void **)&device_bias, channel_out * sizeof(float)));
-    CHECK(cudaMalloc((void **)&device_unroll_matrix, height_out * width_out * channel_in * height_kernel * width_kernel * sizeof(float)));
 
     // Copy input and mask data to device
-    CHECK(cudaMemcpy(device_input, input_data, n_samples * channel_in * height_in * width_in * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_weight, weight_data, channel_out * channel_in * height_kernel * width_kernel * sizeof(float), cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(device_bias, bias_data, channel_out * sizeof(float), cudaMemcpyHostToDevice));
 
-    // // Set the kernel dimensions and call the kernel
-    // int height_grid = (height_out + TILE_WIDTH - 1) / TILE_WIDTH;
-    // int width_grid = (width_out + TILE_WIDTH - 1) / TILE_WIDTH;
-    // int Z = height_grid * width_grid;
-    // dim3 num_threads_per_block(TILE_WIDTH, TILE_WIDTH, 1);
-    // dim3 num_blocks_in_grid(n_samples, output_channel, Z);
+
+
+    //what is this? yes we have n_samples images, but in some case, we cannot load them all into GPU mem,
+    //we seperate them to each batch, an here I set it 32 
+    int batch_size = 32;
+    // setting cuda streams
+    int nStreams = 4;
+    float **device_input = new float*[nStreams], **device_output = new float*[nStreams];
+    cudaStream_t streams[nStreams];
+    
+    // Set the kernel dimensions and call the kernel
     dim3 blockSize_unroll(1024);
-    dim3 gridSize_unroll((height_out * width_out  * channel_in-1)/1024 + 1 );
+    dim3 gridSize_unroll((height_out * width_out  * channel_in-1)/1024 + 1 ,1,batch_size);
 
     dim3 blockSize_multi(32, 32);
-    dim3 gridSize_multi(( channel_out-1)/blockSize_multi.y + 1,(height_out * width_out-1)/blockSize_multi.x + 1,1);
+    dim3 gridSize_multi(( channel_out-1)/blockSize_multi.y + 1,(height_out * width_out-1)/blockSize_multi.x + 1,batch_size);
 
-    //printf("block : 1024, grid : %d\n",(height_out * width_out  * max(channel_in,channel_out)-1)/1024 + 1 );    
 
-    for (int i = 0; i < n_samples; i ++) {
-        unroll_kernel_1<<<gridSize_unroll, blockSize_unroll>>>
+    for (int i = 0; i < nStreams; i++){
+		CHECK(cudaStreamCreate(&streams[i]));    
+        //Each stream use its GPU mem, and no new GPU location
+        CHECK(cudaMalloc((void **)&device_input[i], batch_size * channel_in * height_in * width_in * sizeof(float)));
+        CHECK(cudaMalloc((void **)&device_output[i], batch_size * channel_out * height_out * width_out * sizeof(float)));
+    }
+    
+    CHECK(cudaMalloc((void **)&device_unroll_matrix, batch_size * height_out * width_out * channel_in * height_kernel * width_kernel * sizeof(float)));
+
+    // loop through each sample
+    for (int stream = 0; stream < nStreams; stream++){
+        for (int i = stream * batch_size; i < n_samples; i+=nStreams*batch_size) {
+            //There is a problem. Most of time, the final batch dont have enough image, will it cause error?
+            //The answer is no, because there are still some images from batch before the last
+            int start_in = i * channel_in * height_in * width_in;
+            int start_out = i * channel_out * height_out * width_out;
+            
+            //copy the data to correct stream mem 
+            CHECK(cudaMemcpyAsync(device_input[stream], input_data + start_in, min(batch_size,n_samples-i) * channel_in * height_in * width_in * sizeof(float), cudaMemcpyHostToDevice, streams[stream]));
+
+            unroll_kernel_1<<<gridSize_unroll, blockSize_unroll>>>
                             (channel_in,  height_in,  width_in,  height_kernel, 
                              width_kernel,  height_out,  width_out, 
-                            device_input + i*channel_in * height_in * width_in,  device_unroll_matrix);
+                            device_input[stream],  device_unroll_matrix);
                             
-        multi_weight_add_bias_kernel_1<<<gridSize_multi,blockSize_multi>>>
-                            (device_unroll_matrix,device_weight,device_output + i*channel_out * height_out * width_out,device_bias
-                            ,height_out * width_out, height_kernel * width_kernel * channel_in, channel_out);
+            multi_weight_add_bias_kernel_1<<<gridSize_multi,blockSize_multi>>>
+                                (device_unroll_matrix,device_weight,device_output[stream],device_bias
+                                ,height_out * width_out, height_kernel * width_kernel * channel_in, channel_out);
+                    
+            CHECK(cudaMemcpyAsync(output_data + start_out, device_output[stream], min(batch_size,n_samples-i) * channel_out * height_out * width_out * sizeof(float), cudaMemcpyDeviceToHost, streams[stream]));
+        }
+    }
+    cudaError_t errSync = cudaGetLastError();
+    cudaError_t errAsync = cudaDeviceSynchronize();
+    if (errSync != cudaSuccess) 
+        printf("Sync kernel error: %s\n", cudaGetErrorString(errSync));
+    if (errAsync != cudaSuccess)
+        printf("Async kernel error: %s\n", cudaGetErrorString(errAsync));
+    // Free device memory
+    for (int i = 0; i < nStreams; i++){
+        CHECK(cudaStreamSynchronize(streams[i]));
+        cudaStreamDestroy(streams[i]);
+        //delete each stream GPU mem 
+        CHECK(cudaFree(device_input[i]));
+        CHECK(cudaFree(device_output[i]));
     }
 
-    // Launch the kernel
-    //conv_forward_kernel<<<num_blocks_in_grid, num_threads_per_block>>>(device_output, device_input, device_weight, n_samples, output_channel, channel_in, height_in, width_in, kernel_height);
-    //CHECK(cudaDeviceSynchronize()); // Ensure that the GPU has completed the computation
-
-    // Copy the output back to host
-    CHECK(cudaMemcpy(output_data, device_output, n_samples * channel_out * height_out * width_out * sizeof(float), cudaMemcpyDeviceToHost));
-
-    // Free device memory
-    CHECK(cudaFree(device_input));
-    CHECK(cudaFree(device_output));
+    CHECK(cudaFree(device_bias));
+    
     CHECK(cudaFree(device_weight));
     CHECK(cudaFree(device_unroll_matrix));
 
